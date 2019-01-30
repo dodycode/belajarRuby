21dbc53db0e99bda71173b28371992543846dd48
dodypras <prasetyodody17@gmail.com>
2019-01-30 21:00:56 +0800

    initial commit

---
+++ aritmatika.rb
@@ -0,0 +1,6 @@
+pangkatTiga = 2**3
+
+puts pangkatTiga
+
+puts "Apakah hasilnya genap? Hasil = #{pangkatTiga.even?}"
+puts "Apakah hasilnya ganjil? Hasil = #{pangkatTiga.odd?}"
---
+++ string.rb
@@ -0,0 +1,11 @@
+nama = 'Dazai'
+buku = 'Overlord' 
+
+puts "Buku berjudul #{buku} diciptakan oleh #{nama}"
+
+angka = 10
+angka2 = 2
+hitung = angka * angka2
+
+puts "Coba nulis string pake kutip dua, Hasil = #{hitung}"
+puts 'Coba nulis string pake kutip satu, Hasil = #{hitung}'
